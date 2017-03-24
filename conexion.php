c8f6fb6e5d194aae3ddb4f43510fa026b6d8bbfb
Sufian Saguer Mimon <sufinho10@hotmail.com>
2017-03-24 10:52:41 +0100

    BackUp_24032017
    First Week!

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost="192.168.2.42";  // host del MySQL (generalmente localhost)
+$dbusuario="sufian"; // aqui debes ingresar el nombre de usuario
+                      // para acceder a la base
+$dbpassword="imnewhere"; // password de acceso para el usuario de la
+                      // linea anterior
+$db="sufian";        // Seleccionamos la base con la cual trabajar
+//$conexion = mysql_connect($dbhost, $dbusuario, $dbpassword);
+//mysql_select_db($db, $conexion);
+?>
